f1b4060f1cf643f425708aba3b470ecd05938202
Kaiyaa Yuuki <rizaloke507@gmail.com>
2024-08-03 00:16:50 +0000

    new compiler

--- kaicompile.sh
+++ kaicompile.sh
@@ -1,108 +1,77 @@
 #!/bin/sh
 
-# Many parts of this script were taken from @REIGNZ, @idkwhoiam322 and @raphielscape . Huge thanks to them.
+DEFCONFIG="kaikernel_defconfig"
+CLANGDIR="/workspace/KaiKernel_sdm_845/clang-19"
 
-# Some general variables
-PHONE="beryllium"
-ARCH="arm64"
-SUBARCH="arm64"
-DEFCONFIG=kaikernel_defconfig
-COMPILER=clang
-LINKER=""
-COMPILERDIR="/workspaces/proton-clang"
+#
+rm -rf compile.log
 
-# Outputs
+#
+mkdir -p out
 mkdir out/KaiKernel
-mkdir out/KaiKernel/SE
-mkdir out/KaiKernel/NSE
+mkdir out/KaiKernel/SE_OC
+mkdir out/KaiKernel/NSE_OC
 
-# Export shits
+
+#
 export KBUILD_BUILD_USER=Kaiyaa77
 export KBUILD_BUILD_HOST=Yuu507
+export PATH="$CLANGDIR/bin:$PATH"
 
-# Speed up build process
+#
+make O=out ARCH=arm64 $DEFCONFIG
+
+#
 MAKE="./makeparallel"
 
-# Basic build function
-BUILD_START=$(date +"%s")
+#
+START=$(date +"%s")
 blue='\033[0;34m'
 cyan='\033[0;36m'
 yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 
-Build () {
-PATH="${COMPILERDIR}/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-ARCH=${ARCH} \
-CC=${COMPILER} \
-CROSS_COMPILE=${COMPILERDIR}/bin/aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=${COMPILERDIR}/bin/arm-linux-gnueabi- \
-LD_LIBRARY_PATH=${COMPILERDIR}/lib
-}
-
-Build_lld () {
-PATH="${COMPILERDIR}/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-ARCH=${ARCH} \
-CC=${COMPILER} \
-CROSS_COMPILE=${COMPILERDIR}/bin/aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=${COMPILERDIR}/bin/arm-linux-gnueabi- \
-LD=ld.${LINKER} \
+rve () {
+make -j$(nproc --all) O=out LLVM=1 \
+ARCH=arm64 \
+CC=clang \
+LD=ld.lld \
 AR=llvm-ar \
+AS=llvm-as \
 NM=llvm-nm \
 OBJCOPY=llvm-objcopy \
 OBJDUMP=llvm-objdump \
 STRIP=llvm-strip \
-ld-name=${LINKER} \
-KBUILD_COMPILER_STRING="Proton Clang"
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 }
-
-# Make defconfig
-
-make O=out ARCH=${ARCH} ${DEFCONFIG}
-if [ $? -ne 0 ]
-then
-    echo "Build failed"
-else
-    echo "Made ${DEFCONFIG}"
-fi
-
-# Build starts here
-if [ -z ${LINKER} ]
-then
-        #NEW-SE
-        cp KaiKernel/TOUCH_FW/OLD/* firmware/
+        #SE Overclock
         cp KaiKernel/SE/* arch/arm64/boot/dts/qcom/
-        cp KaiKernel/OC/gpucc-sdm845.c drivers/clk/qcom/
-        cp KaiKernel/OC/sdm845-v2.dtsi arch/arm64/boot/dts/qcom/
-        Build 2>&1 | tee -a compile.log
-else
-        Build_lld 2>&1 | tee -a compile.log
-fi
+        cp KaiKernel/OC/818/sdm845-v2.dtsi arch/arm64/boot/dts/qcom/
+        cp KaiKernel/OC/818/gpucc-sdm845.c drivers/clk/qcom/
+        rve
         if [ $? -ne 0 ]
         then
             echo "Build failed"
-            rm -rf out/KaiKernel/SE/*
         else
             echo "Build succesful"
-            cp out/arch/arm64/boot/Image.gz-dtb out/KaiKernel/SE/Image.gz-dtb
-
-            #NEW-NSE
+            cp out/arch/arm64/boot/Image.gz-dtb out/KaiKernel/SE_OC/Image.gz-dtb
+            
+            #NSE Overclock
             cp KaiKernel/NSE/* arch/arm64/boot/dts/qcom/
-            Build
+            cp KaiKernel/OC/818/sdm845-v2.dtsi arch/arm64/boot/dts/qcom/
+            cp KaiKernel/OC/818/gpucc-sdm845.c drivers/clk/qcom/
+            rve
             if [ $? -ne 0 ]
             then
                 echo "Build failed"
-                rm -rf out/KaiKernel/NSE/*
             else
                 echo "Build succesful"
-                cp out/arch/arm64/boot/Image.gz-dtb out/KaiKernel/NSE/Image.gz-dtb
-            fi
-        fi
+                cp out/arch/arm64/boot/Image.gz-dtb out/KaiKernel/NSE_OC/Image.gz-dtb
+    fi
+fi
 
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
+END=$(date +"%s")
+DIFF=$(($END - $START))
 echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
-
-
